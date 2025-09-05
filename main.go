638059a186b55b9c32bc74e84d961f235c035d41
MatheoDarlo <Matheo.Darlo@ynov.com>
2025-09-05 04:32:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Helllllllo, World!")
+	fmt.Fprintf(w, "suce ma kekette!")
 }
 func main() {
 	http.HandleFunc("/", handler)
