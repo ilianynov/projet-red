116d75d99a7decc83c52f464f015ddacac3f1533
ilianynov <ilian.briki@ynov.com>
2025-09-08 17:42:24 +0200

    Update character.go

--- character/character.go
+++ character/character.go
@@ -1,6 +1,5 @@
 package main
 
-// No imports needed for this file currently
 import "fmt"
 
 type Character struct {
@@ -12,6 +11,18 @@ type Character struct {
 	Inventaire []string
 }
 
+// Nouvelle fonction pour initialiser un personnage
+func initCharacter(nom string, classe string, niveau int, maxHP int, hpActuel int, inventaire []string) Character {
+	return Character{
+		Nom:        nom,
+		Classe:     classe,
+		Niveau:     niveau,
+		MaxHP:      maxHP,
+		HPactuel:   hpActuel,
+		Inventaire: inventaire,
+	}
+}
+
 func CharacterCreation() {
 	char1 := Character{
 		Nom:        "Elfe",
@@ -21,7 +32,11 @@ func CharacterCreation() {
 		HPactuel:   100,
 		Inventaire: []string{},
 	}
-	fmt.Println(char1)
+	fmt.Println("Personnage char1 :", char1)
+
+	// Création d'un autre personnage c1 avec initCharacter
+	c1 := initCharacter("VotreNom", "Elfe", 1, 100, 40, []string{"Potion", "Potion", "Potion"})
+	fmt.Println("Personnage c1 :", c1)
 }
 
 func main() {
