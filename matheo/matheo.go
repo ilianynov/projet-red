41f775ffeea7095070ef8c4f0252f1b148e22b69
MatheoDarlo <Matheo.Darlo@ynov.com>
2025-09-08 04:55:36 -0400

    ut

--- matheo/matheo.go
+++ matheo/matheo.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func isDead(c * Character ) bool {
+// poisonPot deals 10 damage per second for 3 seconds, printing HP each time
+func poisonPot(c *Character) {
+	for i := 0; i < 3; i++ {
+		c.HP -= 10
+		if c.HP < 0 {
+			c.HP = 0
+		}
+		fmt.Printf("HP: %d/%d\n", c.HP, c.MaxHP)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func isDead(c *Character) bool {
 	if c.HP <= 0 {
 		fmt.Println("je vous croyais plus fort... Résurrection ?")
 		c.HP = c.MaxHP / 2
@@ -10,13 +25,3 @@ func isDead(c * Character ) bool {
 	}
 	return false
 }
-
-import ("time")
-	
-
-Time.sleep(1 * Time.Second)
-
-func poisonPot(c *character) {
-	
-}
-
