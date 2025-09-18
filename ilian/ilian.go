86b11bb263700f240ab01ed7ea6acb6fc7985b49
ilianynov <ilian.briki@ynov.com>
2025-09-18 09:14:29 +0200

    Update ilian.go

--- ilian/ilian.go
+++ ilian/ilian.go
@@ -1,74 +1,105 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
-func main() {
-	Name()
-	Classe()
+type Character struct {
+	Name      string
+	Class     string
+	Level     int
+	MaxHP     int
+	CurrentHP int
+	Inventory []string
 }
 
-func Name() string {
-	var i string
-
-	fmt.Println("=====================================")
-	fmt.Println("| Entrez le nom de votre personnage |")
-	fmt.Println("=====================================")
-	fmt.Scan(&i)
-	fmt.Println("========================================")
-	fmt.Println("| Bienvenue dans notre monde,", i, " ! |")
-	fmt.Println("========================================")
-	return ""
-}
-
-var ClasseValue string
-
-func Classe() string {
-	var r int
-
-	fmt.Println("  _______________________________________________________________")
-	fmt.Println(" /                                                               \\")
-	fmt.Println("|      						      		  |")
-	fmt.Println("|                    //===================\\                       |")
-	fmt.Println("|                    |      CHOISISSEZ     |                      |")
-	fmt.Println("|                    |                     |                      |")
-	fmt.Println("|                    |         VOTRE       |                      |")
-	fmt.Println("|                    |                     |                      |")
-	fmt.Println("|                    |        CLASSE       |                      |")
-	fmt.Println("|                    \\===================//                       |")
-	fmt.Println("|                                                                 |")
-	fmt.Println("|                                                                 |")
-	fmt.Println("|_________________________________________________________________|")
-	fmt.Println(" \\                                                               /")
-	fmt.Println("  )                                                             ( ")
-	fmt.Println(" /                                                               \\")
-	fmt.Println("|		     1. Humain        4. Nain                     |")
-	fmt.Println("|   	             2. Loup-garou    5. Ange                     |")
-	fmt.Println("|   		     3. Hybride       6. Démon                    |")
-	fmt.Println(" \\_______________________________________________________________/")
-
-	fmt.Scan(&r)
-
-	if r == 1 {
-		fmt.Println("Vous avez choisi la classe Humain")
-		ClasseValue = "Humain"
-	} else if r == 2 {
-		fmt.Println("Vous avez choisi la classe Loup-garou")
-		ClasseValue = "Loup-garou"
-	} else if r == 3 {
-		fmt.Println("Vous avez choisi la classe Hybride")
-		ClasseValue = "Hybride"
-	} else if r == 4 {
-		fmt.Println("Vous avez choisi la classe Nain")
-		ClasseValue = "Nain"
-	} else if r == 5 {
-		fmt.Println("Vous avez choisi la classe Ange")
-		ClasseValue = "Ange"
-	} else if r == 6 {
-		fmt.Println("Vous avez choisi la classe Démon")
-		ClasseValue = "Démon"
+func initCharacter(name, class string, level, maxHP, currentHP int, inventory []string) Character {
+	return Character{
+		Name:      name,
+		Class:     class,
+		Level:     level,
+		MaxHP:     maxHP,
+		CurrentHP: currentHP,
+		Inventory: inventory,
 	}
+}
 
-	return ""
+func displayInfo(c Character) {
+	fmt.Printf("Nom: %s\nClasse: %s\nNiveau: %d\nHP: %d/%d\nInventaire: %v\n",
+		c.Name, c.Class, c.Level, c.CurrentHP, c.MaxHP, c.Inventory)
+}
+
+func accessInventory(c *Character) {
+	if len(c.Inventory) == 0 {
+		fmt.Println("Inventaire vide")
+		return
+	}
+	for i, item := range c.Inventory {
+		fmt.Printf("%d - %s\n", i+1, item)
+	}
+	fmt.Println("0 - Retour")
+	var choice int
+	fmt.Scanln(&choice)
+	if choice > 0 && choice <= len(c.Inventory) {
+		if c.Inventory[choice-1] == "Potion" {
+			takePot(c, choice-1)
+		}
+	}
+}
+
+func takePot(c *Character, index int) {
+	c.Inventory = append(c.Inventory[:index], c.Inventory[index+1:]...)
+	c.CurrentHP += 50
+	if c.CurrentHP > c.MaxHP {
+		c.CurrentHP = c.MaxHP
+	}
+	fmt.Printf("Vous avez bu une potion. HP: %d/%d\n", c.CurrentHP, c.MaxHP)
+}
+
+func addInventory(c *Character, item string) {
+	c.Inventory = append(c.Inventory, item)
+	fmt.Printf("%s ajouté à l'inventaire\n", item)
+}
+
+func merchant(c *Character) {
+	fmt.Println("Marchand :")
+	fmt.Println("1 - Potion de vie (gratuit)")
+	fmt.Println("0 - Retour")
+	var choice int
+	fmt.Scanln(&choice)
+	if choice == 1 {
+		addInventory(c, "Potion")
+	}
+}
+
+func menu(c *Character) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("\nMenu:")
+		fmt.Println("1 - Afficher les informations")
+		fmt.Println("2 - Accéder à l'inventaire")
+		fmt.Println("3 - Marchand")
+		fmt.Println("0 - Quitter")
+		fmt.Print("Choix: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		switch input {
+		case "1":
+			displayInfo(*c)
+		case "2":
+			accessInventory(c)
+		case "3":
+			merchant(c)
+		case "0":
+			return
+		}
+	}
+}
+
+func main() {
+	c1 := initCharacter("Alan", "Elfe", 1, 100, 40, []string{"Potion", "Potion", "Potion"})
+	menu(&c1)
 }
