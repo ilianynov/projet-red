e97eebfe058a6109fe969450f0c85a6b5d898937
MatheoDarlo <Matheo.Darlo@ynov.com>
2025-09-18 14:37:46 -0400

    ef

--- graphic.go
+++ graphic.go
@@ -1,7 +1,7 @@
 package main
 
 func ShowDeathArtGraphic() {
-	println(`..--======---:...                                        
+	println(                               `..--======---:...                                        
                                       ..-+=-:....    ...::-+=..                                     
                                    .:*+.               ..   .-#+.                                   
                                 .:+=...                     -...-*-.                                
